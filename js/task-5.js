b4abe82b878829e3cda7641ee61bddffba190019
Timoshchyk <timosuko202@gmail.com>
2024-01-16 19:02:10 +0200

    hw-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorBtn = document.querySelector(".change-color");
+const background = document.querySelector("body");
+const colorText = document.querySelector(".color");
+colorBtn.addEventListener("click", changeBackgroundColor);
+function changeBackgroundColor(){
+  const newColor = getRandomHexColor();
+  background.style.backgroundColor = newColor;
+  colorBtnText.textContent = newColor;
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputNumber = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-desroy]");
+const boxElem = document.querySelector("#boxes");
+createBtn.addEventListener("click", onCreateBtnClick);
+destroyBtn.addEventListener("click", onDestroyBtnClick);
+const beginningWidth = 30;
+const beginningHeight = 30;
+function onCreateBtnClick() {
+  onDestroyBtnClick();
+  let width = beginningWidth;
+  let height = beginningHeight;
+  let arr = [];
+  const number = +inputNumber.value;
+  if (number >= 1 && number <= 100) {
+    for (let i = 1; i <= number; i++) {
+      arr.push(
+        `<div style="width: ${width}px; heigth: ${heigth}px; background-color: ${getRandomHexColor()}"></div>`
+      );
+      width +=10;
+      heigth += 10;
+    }
+  }
+  boxElem.insertAdjacentHTML("afterbegin", arr.join(""));
+}
+function onDestroyBtnClick(){
+  while (boxElem.firstChild){
+    boxElem.removeChild(boxElem.firstChild);
+  }
+}
