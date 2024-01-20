a089f0492140d5392009f520a92bbb36c0092412
Timoshchyk <timosuko202@gmail.com>
2024-01-20 11:50:27 +0200

    hw-6

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#name-input");
 const greeting = document.querySelector("#name-output");
-input.addEventListener("input", onInputBlur);
+input.addEventListener("input", onInputEvent);
 function onInputEvent(event) {
     const name = input.value.trim();
     if (name === "") {
